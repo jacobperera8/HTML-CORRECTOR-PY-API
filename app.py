aadfe3556833ab78d907f82b68aec9d49e33f92a
William-Boomer-Baker <boomer@boomerbaker.com>
2022-10-18 21:14:44 +0530

    success

--- app.py
+++ app.py
@@ -10,27 +10,34 @@ app = Flask(__name__)
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=3000, debug=True)
 
-# @app.route("/")
-# def hello_from_root():
-#     return jsonify(message='Hello from root!')
-#
-#
-# @app.route("/hello")
-# def hello():
-#     return jsonify(message='Hello from path!')
-
-
-# app = Flask(__name__)
-
 @app.route("/")
 def hello_from_root():
     return jsonify(message='Hello from root!')
 
 
-@app.route("/hello")
-def hello():
-    return jsonify(message='Hello from path!')
+@app.route('/post-process-save', methods=['POST'])
+def insert2():
+    if request.method == "POST":
+        url_key = request.get_json(silent=True)['urlKey']
+        dir = request.get_json(silent=True)['dir']
+        s3 = boto3.client('s3')
+        bucket_name = 'html-corrector-bucket'
+        object_key = url_key
+        obj = s3.get_object(Bucket=bucket_name, Key=object_key)
 
+        df = pd.read_excel(io.BytesIO(obj['Body'].read()))
+
+        print(df.head())
+
+        csv_buffer = StringIO()
+        df.to_csv(csv_buffer)
+        s3_resource = boto3.resource('s3')
+        s3_resource.Object(bucket_name, 'pre-processed-data/df.csv').put(Body=csv_buffer.getvalue())
+
+        return jsonify(message='completed postprocessing the dataset!',
+                       urlKey=url_key,
+                       s3Dir=dir,
+                       savedDir='pre-processed-data/df.csv')
 
 @app.route('/process', methods=['POST'])
 def insert():
@@ -412,7 +419,17 @@ def insert():
         # for x in df.index:
         #   preprocess_rgba_with_4_px_values(x, ["box-shadow"])
 
-        return jsonify(message='completed preprocessing the dataset!', urlKey=url_key, s3Dir=dir)
+        # return jsonify(message='completed preprocessing the dataset!', urlKey=url_key, s3Dir=dir)
+
+        csv_buffer = StringIO()
+        df.to_csv(csv_buffer)
+        s3_resource = boto3.resource('s3')
+        s3_resource.Object(bucket_name, 'pre-processed-data/df.csv').put(Body=csv_buffer.getvalue())
+
+        return jsonify(message='completed postprocessing the dataset!',
+                       urlKey=url_key,
+                       s3Dir=dir,
+                       savedDir='pre-processed-data/df.csv')
 
 
 @app.errorhandler(404)
